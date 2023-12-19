0fdaf4a1edceea8dd1e834685674985e5f6381a9
irwin-rex <irwin.a@equipohealth.com>
2023-12-19 12:39:33 +0530

    New Project for SWARM

--- playcode/urls.py
+++ playcode/urls.py
@@ -1,7 +1,8 @@
-"""testIrwin URL Configuration
+"""
+URL configuration for playcode project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from . import views
+
+from playcode import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', views.HomeView.as_view()),
+    path("admin/", admin.site.urls),
+        path('home/', views.HomeView.as_view()),
 ]
