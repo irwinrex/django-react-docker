72399884a13407177b203b323df72b93960de5ed
irwin-rex <irwin.a@equipohealth.com>
2024-03-01 08:49:45 +0530

    updated django

--- playcode/settings.py
+++ playcode/settings.py
@@ -36,6 +36,7 @@ CORS_ORIGIN_ALLOW_ALL=False
 # Application definition
 
 INSTALLED_APPS = [
+    "playcodeapp",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders'
 
 ]
@@ -132,13 +134,26 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #Rest Framework Configuration
+# REST_FRAMEWORK = {
+# 'DEFAULT_RENDERER_CLASSES': [
+#     # 'rest_framework.renderers.JSONRenderer',
+#     'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     # 'rest_framework.authentication.SessionAuthentication',
+#     # 'rest_framework.authentication.BasicAuthentication',
+# ],
+# # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
+# # 'DEFAULT_SCHEMA_CLASS':  'rest_framework.schemas.coreapi.AutoSchema',
+# }
 REST_FRAMEWORK = {
-'DEFAULT_RENDERER_CLASSES': [
-    'rest_framework.renderers.JSONRenderer',
-],
-'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
-'DEFAULT_SCHEMA_CLASS':  'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
+
 
 with open('secrets.json') as secrets_file:
     secrets = json.load(secrets_file)
--- playcodeapp/urls.py
+++ playcodeapp/urls.py
@@ -14,9 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import include
 
 from playcodeapp import views
 
@@ -29,10 +27,10 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('api/', include('playcodeapp.urls')),
-    # path('home/', views.HomeView.as_view()),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # path("admin/", admin.site.urls),
+    # # path('api/', include('playcodeapp.urls')),
+    path('home/', views.HomeView.as_view()),
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
